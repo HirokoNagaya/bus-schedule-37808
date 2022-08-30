d9551112c3c5bd510282da73846cc3eec233230c
pikopikotaro <ringo-hunter-dreamer.44.121@docomo.ne.jp>
2022-08-30 09:52:05 +0900

    アプリ名変更

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BusWorkSchedule
+module BusSchedule37808
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "bus_work_schedule_production"
+  # config.active_job.queue_name_prefix = "bus_schedule37808_production"
 
   config.action_mailer.perform_caching = false
 
