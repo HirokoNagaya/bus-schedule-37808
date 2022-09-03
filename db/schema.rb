f950ee7edd1471b82c776483a100ddb6ed038f85
pikopikotaro <ringo-hunter-dreamer.44.121@docomo.ne.jp>
2022-09-03 15:12:49 +0900

    roomsテーブルの作成
    メッセージモデルは削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_31_012255) do
+ActiveRecord::Schema.define(version: 2022_09_03_060303) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :message do
+  factory :post do
     
   end
 end
