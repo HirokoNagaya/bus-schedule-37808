99c4a37a6d3020781e4c9f121f00ea553ee9c918
pikopikotaro <ringo-hunter-dreamer.44.121@docomo.ne.jp>
2022-09-07 10:27:12 +0900

    messagesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_04_002116) do
+ActiveRecord::Schema.define(version: 2022_09_07_011617) do
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_messages_on_post_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "post_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "post_id", null: false
@@ -41,6 +51,8 @@ ActiveRecord::Schema.define(version: 2022_09_04_002116) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "posts"
+  add_foreign_key "messages", "users"
   add_foreign_key "post_users", "posts"
   add_foreign_key "post_users", "users"
 end
