037afbc9764bd6b6aaa4996b6178c7fa0d3465cd
pikopikotaro <ringo-hunter-dreamer.44.121@docomo.ne.jp>
2022-09-09 11:10:46 +0900

    メッセージの表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tops#index"
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:new, :create, :index] do
+    resources :messages, only: [:index, :create]
+  end
 end
