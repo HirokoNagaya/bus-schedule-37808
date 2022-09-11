a2603661f87b5c4a904f3b753154909414e02e02
pikopikotaro <ringo-hunter-dreamer.44.121@docomo.ne.jp>
2022-09-11 14:43:43 +0900

    googleスケジュールの埋め込み

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,7 @@
 class TopsController < ApplicationController
   def index
   end
+
+  def schedule
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tops#index"
+  get "tops/schedule"
   resources :posts, only: [:new, :create, :index, :destroy] do
     resources :messages, only: [:index, :create]
   end
